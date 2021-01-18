6834e43fe54a7ac54eeabd4259c190ebf2b9c8ef
Jon Curtis <joncurtis@Jons-MacBook-Pro.local>
2021-01-18 12:51:00 +0000

    Fixing formatting

--- src/test/java/com/curtis/tracking/trackingwebsever/TrackingWebserverApplicationTests.java
+++ src/test/java/com/curtis/tracking/trackingwebsever/TrackingWebserverApplicationTests.java
@@ -23,65 +23,58 @@ import com.curtis.tracking.trackingwebserver.handlers.TrackingHandler;
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT, classes = TrackingWebserverApplication.class)
 public class TrackingWebserverApplicationTests {
-	
+
 	@Rule
 	public TemporaryFolder folder = new TemporaryFolder();
-	
-    @Test
-    public void imgRouteShouldReturnOKAndPNG() throws IOException {
-    	TrackingHandler trackingHandler = new TrackingHandler("transparent-pixel.gif");
-    	TrackingWebserverApplication application = new TrackingWebserverApplication(trackingHandler, null);
+
+	@Test
+	public void imgRouteShouldReturnOKAndPNG() throws IOException {
+		TrackingHandler trackingHandler = new TrackingHandler("transparent-pixel.gif");
+		TrackingWebserverApplication application = new TrackingWebserverApplication(trackingHandler, null);
 
 		Resource resource = new ClassPathResource("transparent-pixel.gif");
-        InputStream inputStream = resource.getInputStream();
+		InputStream inputStream = resource.getInputStream();
 		byte[] gif = IOUtils.toByteArray(inputStream);
-    	
-    	final WebTestClient client = WebTestClient
-            .bindToRouterFunction(application.getImgRoute())
-            .build();
 
-        client.get()
-            .uri("/img")
-            .exchange()
-            .expectStatus()
-            .isOk()
-            .expectHeader()
-            .contentType(MediaType.IMAGE_PNG)
-            .expectBody(byte[].class)
-            .isEqualTo(gif);
-    }
-	
-    @Test
-    public void pingRouteShouldReturnOKWhenFileIsPresent() throws IOException {
-    	PingHandlerFilter pingHandlerFilter = new PingHandlerFilter(folder.newFile("ok").getAbsolutePath());
-    	TrackingWebserverApplication application = new TrackingWebserverApplication(null, pingHandlerFilter);
-    	
-    	final WebTestClient client = WebTestClient
-            .bindToRouterFunction(application.getPingRoute())
-            .build();
+		final WebTestClient client = WebTestClient.bindToRouterFunction(application.getImgRoute()).build();
 
-        client.get()
-            .uri("/ping")
-            .exchange()
-            .expectStatus()
-            .isOk()
-            .expectBody(String.class)
-            .isEqualTo("OK");
-    }
-    
-    @Test
-    public void pingRouteShouldReturn503WhenFileIsNotPresent() throws IOException {    	
-    	PingHandlerFilter pingHandlerFilter = new PingHandlerFilter(folder.getRoot().getAbsolutePath() + "/not_ok");
-    	TrackingWebserverApplication application = new TrackingWebserverApplication(null, pingHandlerFilter);
-    	
-    	final WebTestClient client = WebTestClient
-            .bindToRouterFunction(application.getPingRoute())
-            .build();
+		client.get()
+			.uri("/img")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectHeader()
+			.contentType(MediaType.IMAGE_PNG)	
+			.expectBody(byte[].class).isEqualTo(gif);
+	}
 
-        client.get()
-            .uri("/ping")
-            .exchange()
-            .expectStatus()
-            .isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
-    }
+	@Test
+	public void pingRouteShouldReturnOKWhenFileIsPresent() throws IOException {
+		PingHandlerFilter pingHandlerFilter = new PingHandlerFilter(folder.newFile("ok").getAbsolutePath());
+		TrackingWebserverApplication application = new TrackingWebserverApplication(null, pingHandlerFilter);
+
+		final WebTestClient client = WebTestClient.bindToRouterFunction(application.getPingRoute()).build();
+
+		client.get()
+			.uri("/ping")
+			.exchange()
+			.expectStatus()
+			.isOk()
+			.expectBody(String.class)
+			.isEqualTo("OK");
+	}
+
+	@Test
+	public void pingRouteShouldReturn503WhenFileIsNotPresent() throws IOException {
+		PingHandlerFilter pingHandlerFilter = new PingHandlerFilter(folder.getRoot().getAbsolutePath() + "/not_ok");
+		TrackingWebserverApplication application = new TrackingWebserverApplication(null, pingHandlerFilter);
+
+		final WebTestClient client = WebTestClient.bindToRouterFunction(application.getPingRoute()).build();
+
+		client.get()
+		.uri("/ping")
+			.exchange()
+			.expectStatus()
+			.isEqualTo(HttpStatus.SERVICE_UNAVAILABLE);
+	}
 }
